f512d7f970dc53c30d9fd869831fc7c480a473a1
Leandro Steffens de Oliveira <84768349+LeandroSteffens@users.noreply.github.com>
2024-12-10 16:52:03 -0400

    implementação_completa

---
+++ src/main/java/br/com/leandro/fipe/model/Dados.java
@@ -0,0 +1,4 @@
+package br.com.leandro.fipe.model;
+
+public record Dados(String codigo, String nome) {
+}
---
+++ src/main/java/br/com/leandro/fipe/principal/Principal.java
@@ -0,0 +1,86 @@
+package br.com.leandro.fipe.principal;
+
+import br.com.leandro.fipe.model.Dados;
+import br.com.leandro.fipe.model.Modelos;
+import br.com.leandro.fipe.model.Veiculo;
+import br.com.leandro.fipe.services.ConsumoAPI;
+import br.com.leandro.fipe.services.ConverteDados;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Principal {
+    private Scanner scanner = new Scanner(System.in);
+    private final String URL_BASE ="https://parallelum.com.br/fipe/api/v1/";
+    private ConsumoAPI consumo = new ConsumoAPI();
+    private ConverteDados conversor = new ConverteDados();
+
+    public void exibeMenu(){
+        var menu = """
+                *** OPÇÕES ***
+                Carro
+                Moto
+                Caminhão
+                
+                Digite uma das opções para consulta:
+                """;
+
+        System.out.println(menu);
+        var opcao = scanner.nextLine();
+        String endereco;
+        if(opcao.toLowerCase().contains("carr")){
+            endereco = URL_BASE + "carros/marcas";
+        } else if (opcao.toLowerCase().contains("mot")) {
+            endereco = URL_BASE + "motos/marcas";
+        } else {
+            endereco = URL_BASE + "caminhoes/marcas";
+        }
+
+        var json = consumo.obterDados(endereco);
+        System.out.println(json);
+        var marcas = conversor.obterLista(json, Dados.class);
+        marcas.stream()
+                .sorted(Comparator.comparing(Dados::codigo))
+                .forEach(System.out::println);
+
+        System.out.println("Informe o código da marca: ");
+        var codigoMarca = scanner.nextLine();
+
+        endereco = endereco + "/" + codigoMarca + "/modelos";
+        json = consumo.obterDados(endereco);
+        var modeloLista = conversor.obterDados(json, Modelos.class);
+        System.out.println("\nModelos dessa marca: ");
+        modeloLista.modelos().stream()
+                .sorted(Comparator.comparing(Dados::codigo))
+                .forEach(System.out::println);
+
+        System.out.println("\nDigite um trecho do nome do carro: ");
+        var nomeVeiculo = scanner.nextLine();
+
+        List<Dados> modelosFiltrados = modeloLista.modelos().stream()
+                .filter(m -> m.nome().toLowerCase().contains(nomeVeiculo.toLowerCase()))
+                .collect(Collectors.toList());
+        System.out.println("\nModelos filtrados: ");
+        modelosFiltrados.forEach(System.out::println);
+
+        System.out.println("\nDigite o codigo do modelo: ");
+        var codigoModelo = scanner.nextLine();
+
+        endereco = endereco + "/" + codigoModelo + "/anos";
+        json = consumo.obterDados(endereco);
+        List<Dados> anos = conversor.obterLista(json, Dados.class);
+        List<Veiculo> veiculos = new ArrayList<>();
+
+        for(int i = 0; i < anos.size(); i++){
+            var enderecoAno = endereco + "/" + anos.get(i).codigo();
+            json = consumo.obterDados(enderecoAno);
+            Veiculo veiculo = conversor.obterDados(json, Veiculo.class);
+            veiculos.add(veiculo);
+        }
+        System.out.println("\nTodos os veiculos filtrados: ");
+        veiculos.forEach(System.out::println);
+    }
+}
